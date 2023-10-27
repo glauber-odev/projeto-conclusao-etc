f875ecc6dac97b1a163788af9f2a6bcc11deb43f
lucasykw <144175948+lucasykw@users.noreply.github.com>
2023-10-27 11:40:32 -0300

    Add files via upload

---
+++ usuarioPersonal/usuarioPersonal/crud-personal/usuarioPersonal/usuarioPersonal/crud-personal/crud/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste CRUD</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    
+    <header>
+        <h1>O L I M P O</h1>
+    </header>
+    <nav>
+        <a href="#">Home</a>
+        <a href="usuarios/index.php">Usuários</a>
+        <a href="#">Perfil</a>
+    </nav>
+
+</body>
+
+</html>
